492fc601c8518eecbbf241a048db8f8808423ce2
hibah321 <hbhshahbaz321@gmail.com>
2021-06-08 19:42:35 +0400

    added javascript file

---
+++ validation.js
@@ -0,0 +1,16 @@
+window.onload = function(){ 
+    const first = document.getElementById("name0")
+    const name = document.getElementById("name");
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+    const btn = document.querySelector("button");
+    btn.addEventListener("click",()=>{
+        const firstv = first.value
+        const namev = name.value;
+        const emailv = email.value;
+        const passwordv = password.value;
+        
+    });
+
+
+}
